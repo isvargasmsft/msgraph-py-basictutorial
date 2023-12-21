c0d2fabe6e4119bdc32f6cfb7145dc99420ffde1
Isaac Vargas <isvargas@microsoft.com>
2023-12-21 08:20:54 -0600

    new format

--- app.py
+++ app.py
@@ -8,20 +8,21 @@ from kiota_abstractions.api_error import APIError
 from msgraph import GraphServiceClient
 
 credential = ClientSecretCredential("517c3bf7-db40-48f4-95ef-479526a937bc",
-                                    "0fd7031c-6c40-415d-bb6a-ec2e52ade51d",
-                                    "-kE8Q~OAHTfzmJ-aPIChDbJwG2-3M0egMQ3kOcl.")
+                                    "b9122b4d-9b88-4cdf-9cec-ac90c593cf05",
+                                    "1W.8Q~VJ_3ak_l3TeEGKbgK2lFtm0TRra.UFjbUj")
 scopes = ['https://graph.microsoft.com/.default']
 
 client = GraphServiceClient(credential, scopes)
 
-async def get_user_chats():
+async def get_user_messages():
     try:
-        messages = await client.users.by_user_id("AlexW@M365x86781558.OnMicrosoft.com").chats.get()
+        messages = await client.users.by_user_id("AlexW@M365x86781558.OnMicrosoft.com").messages.get()
 
         for msg in messages.value:
             print(
-                msg.topic
+                msg.subject
             )
     except Exception as e_rr:
         print(f'Error: {e_rr.error.message}')
-asyncio.run(get_user_chats())
+        
+asyncio.run(get_user_messages())
